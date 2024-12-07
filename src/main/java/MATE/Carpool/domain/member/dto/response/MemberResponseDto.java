354f1c874a8adac696a2c69c292abfd5ac2ff034
kimhyunjun-55 <mical154@naver.com>
2024-12-08 03:05:48 +0900

    feat : memberService SignUp & SignIn

--- src/main/java/MATE/Carpool/domain/member/dto/response/MemberResponseDto.java
+++ src/main/java/MATE/Carpool/domain/member/dto/response/MemberResponseDto.java
@@ -5,12 +5,14 @@ import MATE.Carpool.domain.member.entity.Member;
 import MATE.Carpool.domain.member.entity.MemberType;
 import MATE.Carpool.domain.member.entity.ProviderType;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Setter
+@Getter
 public class MemberResponseDto extends MemberDto {
 
     private String id;
@@ -19,6 +21,7 @@ public class MemberResponseDto extends MemberDto {
     private Boolean isDriver;
 
     public MemberResponseDto(String encryptId,Member member) {
+        super(member);
         this.id=encryptId;
         this.providerType=member.getProviderType();
         this.memberType=member.getMemberType();
