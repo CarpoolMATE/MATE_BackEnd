0f4077fedc8d3c5bea345ddb5c3ed730460d5f2e
kimhyunjun-55 <mical154@naver.com>
2024-12-08 21:19:20 +0900

    testCommit

---
+++ src/main/java/MATE/Carpool/domain/admin/controller/AdminController.java
@@ -0,0 +1,10 @@
+package MATE.Carpool.domain.admin.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AdminController {
+    public String tets(){
+        return "hellow";
+    }
+}
