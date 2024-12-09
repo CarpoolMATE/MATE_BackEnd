d026fdd0455030e17b6e4d8d663a638e900d16dd
kimhyunjun-55 <mical154@naver.com>
2024-12-10 01:59:18 +0900

    feat : getMember & checkDuplicateEmail

--- src/main/java/MATE/Carpool/domain/member/dto/request/SignupRequestDto.java
+++ src/main/java/MATE/Carpool/domain/member/dto/request/SignupRequestDto.java
@@ -13,15 +13,18 @@ import java.time.LocalDate;
 @AllArgsConstructor
 public class SignupRequestDto {
 
+    @Size(min = 3, max = 20, message = "아이디는는 3자 이상 20자 이하로 가능합니다.")
+    @Pattern(regexp = "^[a-zA-Z]*$", message = "아이디는 3~20자 영문을 사용하세요.")
     private String memberId;
 
-//    @Email
-//    @Pattern(regexp = "^[_a-z0-9-]+(.[_a-z0-9-]+)*@(?:\\w+\\.)+\\w+$", message = "올바른 형식의 이메일을 입력해주세요.")
+    @Email
+    @Pattern(regexp = "^[_a-z0-9-]+(.[_a-z0-9-]+)*@(?:\\w+\\.)+\\w+$", message = "올바른 형식의 이메일을 입력해주세요.")
     private String email;
 
-//    @Size(min = 5, max = 20, message = "비밀번호는 5자 이상 20자 이하로 가능합니다.")
-//    @Pattern(regexp = "^[a-zA-Z\\p{Punct}0-9]*$", message = "비밀번호는 5~20자 영문, 숫자를 사용하세요.")
+    @Size(min = 3, max = 20, message = "비밀번호는 3자 이상 20자 이하로 가능합니다.")
+    @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*\\d)[a-zA-Z0-9]{3,20}$", message = "비밀번호는 3~20자 영문과 숫자를 포함해야 합니다.")
     private String password;
 
     private String nickname;
 }
+
--- src/main/java/MATE/Carpool/domain/member/service/MemberService.java
+++ src/main/java/MATE/Carpool/domain/member/service/MemberService.java
@@ -8,7 +8,6 @@ import MATE.Carpool.config.securityConfig.jwt.RefreshToken;
 import MATE.Carpool.config.securityConfig.jwt.RefreshTokenRepository;
 import MATE.Carpool.config.securityConfig.userDetails.CustomUserDetails;
 import MATE.Carpool.domain.member.dto.request.DriverRequestDto;
-import MATE.Carpool.domain.member.dto.request.MemberRequestDto;
 import MATE.Carpool.domain.member.dto.request.SignInRequestDto;
 import MATE.Carpool.domain.member.dto.response.MemberResponseDto;
 import MATE.Carpool.domain.member.dto.request.SignupRequestDto;
@@ -17,6 +16,7 @@ import MATE.Carpool.domain.member.entity.Member;
 import MATE.Carpool.domain.member.repository.DriverRepository;
 import MATE.Carpool.domain.member.repository.MemberRepository;
 import jakarta.persistence.EntityExistsException;
+import jakarta.persistence.EntityNotFoundException;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
@@ -26,16 +26,10 @@ import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.ErrorResponse;
-import org.springframework.web.server.ResponseStatusException;
-
-import java.security.InvalidParameterException;
-import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -53,18 +47,21 @@ public class MemberService {
     private final RefreshTokenRepository refreshTokenRepository;
 
 
-    public void encrypt(Long number) throws Exception {
-        String encryption = pkEncryption.encrypt(number);
-        String decryption = pkEncryption.decrypt(encryption);
-        System.out.println(encryption);
-        System.out.println(decryption);
-    }
+
 
     //단일멤버조회
-    public ResponseEntity<MemberResponseDto> getMember(String id) {
-        return null;
+    @Transactional(readOnly = true)
+    public ResponseEntity<MemberResponseDto> getMember(String memberId) throws Exception {
+        // 회원 조회 로직 (별도 메서드로 분리)
+        Member member = findByMember(memberId);
+        // ResponseEntity 생성 및 반환
+        MemberResponseDto responseDto = new MemberResponseDto(memberId, member);
+        return new ResponseEntity<>(responseDto, HttpStatus.OK);
     }
 
+
+
+    //로그인
     @Transactional
     public ResponseEntity<Object> signIn(SignInRequestDto requestDto, HttpServletResponse httpServletResponse) throws Exception {
         String memberId = requestDto.getMemberId();
@@ -109,12 +106,12 @@ public class MemberService {
     @Transactional
     public ResponseEntity<String> signUp(SignupRequestDto requestDto) {
 
-        String email = requestDto.getEmail();
+        String memberId = requestDto.getMemberId();
         String password = passwordEncoder.encode(requestDto.getPassword());
-        Optional<Member> findEmail = memberRepository.findByEmail(email);
+        boolean checkId = memberRepository.existsByMemberId(memberId);
 
-        if (findEmail.isPresent()) {
-            throw new EntityExistsException("이미 존재하는 이메일입니다. email : " + email);
+        if (checkId) {
+            throw new EntityExistsException("이미 존재하는 아이디 입니다. Id : " + memberId);
         }
 
         Member member = Member.builder()
@@ -126,8 +123,6 @@ public class MemberService {
 
         memberRepository.save(member);
 
-
-
         return new ResponseEntity<>("회원가입 성공",HttpStatus.OK);
 
     }
@@ -137,10 +132,7 @@ public class MemberService {
     @Transactional
     public ResponseEntity<MemberResponseDto> signUpDriver(DriverRequestDto driverRequestDto) throws Exception {
 
-        Long memberId = decryption(driverRequestDto.getMemberId());
-
-        Member member = memberRepository.findById(memberId)
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "회원이 존재하지 않습니다."));
+        Member member = findByMember(driverRequestDto.getMemberId());
 
         Driver driver = Driver.builder()
                 .carNumber(driverRequestDto.getCarNumber())
@@ -158,14 +150,24 @@ public class MemberService {
         return new ResponseEntity<>(responseDto, HttpStatus.OK);
     }
 
-    private Long decryption(String memberId) throws Exception {
+    @Transactional
+    public Member findByMember(String memberId) throws Exception {
         String decryptedValue = pkEncryption.decrypt(memberId);  // 복호화된 값
         try {
+            Long id =Long.parseLong(decryptedValue);
+            return  memberRepository.findById(id).orElseThrow(()-> new UsernameNotFoundException("회원이 존재하지 않습니다."));
             // 복호화된 값을 Long 타입으로 형변환
-            return Long.parseLong(decryptedValue);
         } catch (NumberFormatException e) {
             throw new Exception("복호화된 값이 숫자 형식이 아닙니다.", e);  // 예외 처리
         }
     }
 
+    @Transactional
+    public ResponseEntity<Boolean> checkEmail(String email) {
+        boolean exists = memberRepository.existsByEmail(email);
+        if (exists) {
+            throw new EntityNotFoundException("중복된 이메일입니다.");
+        }
+        return ResponseEntity.ok(false);
+    }
 }
