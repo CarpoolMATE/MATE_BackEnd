069aac46b0e9b244e1d068a035144b9c8ac7e793
kimhyunjun-55 <mical154@naver.com>
2024-12-29 15:49:24 +0900

    feat : deploy #8

--- src/test/java/MATE/Carpool/domain/admin/controller/AdminControllerTest.java
+++ src/test/java/MATE/Carpool/domain/admin/controller/AdminControllerTest.java
@@ -5,11 +5,10 @@ import MATE.Carpool.common.exception.ErrorCode;
 import MATE.Carpool.domain.member.entity.Member;
 import MATE.Carpool.domain.member.repository.MemberRepository;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.security.authorization.method.AuthorizeReturnObject;
-import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
@@ -22,7 +21,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc
 class AdminControllerTest {
 
-    @Autowired
+    @Mock
     private MemberRepository memberRepository;
 
     //http 시뮬레이션
