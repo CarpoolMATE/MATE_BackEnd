48d7e7e4384096e9b05991ce6ec90e8fe4b3b2d8
KimHyunJun-55 <mical154@naver.com>
2025-01-14 21:14:42 +0900

    Update SecurityConfig.java

--- src/main/java/MATE/Carpool/config/securityConfig/SecurityConfig.java
+++ src/main/java/MATE/Carpool/config/securityConfig/SecurityConfig.java
@@ -95,6 +95,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.addAllowedOrigin("http://localhost:3000");
+        configuration.addAllowedOrigin("http://localhost:3000/");
         configuration.addAllowedOrigin("https://carple-front.vercel.app");
 
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
