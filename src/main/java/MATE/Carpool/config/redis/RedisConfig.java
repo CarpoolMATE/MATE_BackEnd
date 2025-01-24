691c0ef68181a60c2e93930deef732d9da127e05
KimHyunJun-55 <mical154@naver.com>
2025-01-24 19:40:05 +0900

    Update RedisConfig.java

--- src/main/java/MATE/Carpool/config/redis/RedisConfig.java
+++ src/main/java/MATE/Carpool/config/redis/RedisConfig.java
@@ -1,25 +1,40 @@
 package MATE.Carpool.config.redis;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
-
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 
 @Configuration
 public class RedisConfig {
+
+    @Value("${spring.redis.host}")
+    private String redisHost;
+
+    @Value("${spring.redis.port}")
+    private int redisPort;
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        LettuceConnectionFactory factory = new LettuceConnectionFactory(redisHost, redisPort);
+        return factory;
+    }
 
     @Bean
     public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
         RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
         redisTemplate.setConnectionFactory(connectionFactory);
 
+        // Key는 String 형식으로 직렬화
         redisTemplate.setKeySerializer(new StringRedisSerializer());
-        redisTemplate.setValueSerializer(new StringRedisSerializer());
+
+        // Value는 JSON 형식으로 직렬화
+        redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+
         return redisTemplate;
     }
 }
-
-
-
