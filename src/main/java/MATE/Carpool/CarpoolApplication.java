02964a1703cd9a333aa68e613130d55fc9277f85
KimHyunJun-55 <mical154@naver.com>
2025-02-19 21:24:51 +0900

    Update CarpoolApplication.java

--- src/main/java/MATE/Carpool/CarpoolApplication.java
+++ src/main/java/MATE/Carpool/CarpoolApplication.java
@@ -1,9 +1,12 @@
 package MATE.Carpool;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import java.util.TimeZone;
 
 @SpringBootApplication
 @EnableJpaAuditing
@@ -12,6 +15,11 @@ public class CarpoolApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CarpoolApplication.class, args);
+	}
+
+	@PostConstruct
+	public void setTimeZone(){
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
 	}
 
 }
